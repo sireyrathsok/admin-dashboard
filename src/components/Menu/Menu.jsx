ec1da749f34d7129b44cd12e9a3c3351290904db
sireyrathsok <sireyrathsok@gmail.com>
2024-01-28 17:39:31 +0700

    add card animation

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import img1 from "../../assets/icecream.jpg";
 import spa from "../../assets/spa.jpg";
 import ice from "../../assets/ice.jpg";
@@ -39,28 +39,28 @@ const menuList = [
     name: "Spagetti with Shrimp",
     price: "4.5",
     rating: 4,
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: ice,
     name: "Causaul Icecream",
     price: "2.5",
     rating: 3,
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: choIce,
     name: "Chocolate Icecream",
     price: "3.5",
     rating: "4",
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: tacco1,
     name: "Original tacco",
     price: "2.5",
     rating: 3,
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: tacco2,
@@ -102,14 +102,14 @@ const menuList = [
     name: "Spacial Burger",
     price: "5",
     rating: "5",
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: chwing,
     name: "Chicken Wings ",
     price: "3.5",
     rating: "4",
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: milktea,
@@ -123,7 +123,7 @@ const menuList = [
     name: "Tacco Friend Set",
     price: "6.5",
     rating: "5",
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: chFried,
@@ -137,7 +137,7 @@ const menuList = [
     name: "Rice with Omolet",
     price: "4.5",
     rating: "5",
-    popular: true,
+    popular: false,
   },
   {
     imgUrl: doubleBurger,
@@ -254,49 +254,126 @@ const menuList = [
 ];
 
 const Menu = () => {
-  const [PopularisClicked, setPopularisClicked] = useState(false);
+  const [isAllClicked, setAllClicked] = useState(true);
+  const [isPopularClicked, setPopularClicked] = useState(false);
+
+  const handleAllClick = () => {
+    setAllClicked(true);
+    setPopularClicked(false);
+  };
+
+  const handlePopularClick = () => {
+    setAllClicked(false);
+    setPopularClicked(true);
+  };
+
+  // const filteredMenuList = isPopularClicked
+  //   ? menuList.filter((item) => item.popular)
+  //   : menuList;
   return (
-    <div className=" text-white ">
+    <div className=" text-white sm:mb-10 ">
       <div className=" flex justify-start  sm:justify-end gap-5  text-white  mb-5  ">
-        <button className=" bg-orange-400 hover:bg-orange-500 px-6 py-1 rounded-lg ">
+        <button
+          onClick={handleAllClick}
+          className={` bg-orange-400 hover:bg-orange-500 px-6 py-1 rounded-lg ${
+            isAllClicked ? " bg-orange-700" : ""
+          }`}
+        >
           All
         </button>
         <button
-          onClick={() => setPopularisClicked(!false)}
-          className=" bg-orange-400  hover:bg-orange-500 px-6 py-1 rounded-lg "
+          onClick={handlePopularClick}
+          className={` bg-orange-400  hover:bg-orange-500 px-6 py-1 rounded-lg ${
+            isPopularClicked ? " bg-orange-700" : ""
+          }`}
         >
           Popular
         </button>
       </div>
-      <section className=" sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols- gap-8 sm:gap-10 xl:gap-12 overflow-x-hidden">
+      <motion.section
+        layout
+        // initial={{ opacity: 0 }}
+        // animate={{ opacity: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 50,
+        }}
+        exit={{ opacity: 0 }}
+        className=" sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols- gap-8 sm:gap-10 xl:gap-12 overflow-x-hidden"
+      >
         {menuList.map((item, index) => {
-          return (
-            <motion.div
-              whileHover={{ scale: 1.07 }}
-              key={index}
-              className=" mb-16 sm:mb-0"
-            >
-              <img
-                className=" xxs:h-[320px] xs:h-[350px] sm:h-[220px] md:h-56 lg:h-52 xl:h-60 w-full rounded-t-xl"
-                src={item.imgUrl}
-                alt="product"
-              />
-              <div className="  bg-gray-600 px-3 py-3 rounded-b-xl ">
-                <div>
-                  <p className=" text-md text-gray-400">{item.name}</p>
-                </div>
+          {
+            return (
+              <>
+                <AnimatePresence>
+                  <motion.div
+                    // initial={{ opacity: 0 }}
+                    // animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }}
+                    whileHover={{ scale: 1.01 }}
+                    key={index}
+                    className={`mb-16 sm:mb-0  ${isAllClicked ? "" : "hidden"}`}
+                  >
+                    <img
+                      className=" xxs:h-[320px] xs:h-[350px] sm:h-[220px] md:h-56 lg:h-52 xl:h-60 w-full rounded-t-xl"
+                      src={item.imgUrl}
+                      alt="product"
+                    />
+                    <div className="  bg-gray-600 px-3 py-3 rounded-b-xl ">
+                      <div>
+                        <p className=" text-md text-gray-400">{item.name}</p>
+                      </div>
 
-                <div className=" flex justify-between items-center">
-                  <p className=" my-4 text-lg text-gray-300">${item.price}</p>
-                  <p className=" bg-gray-500 hover:bg-gray-400 px-3 py-1  rounded-lg">
-                    Details
-                  </p>
-                </div>
-              </div>
-            </motion.div>
-          );
+                      <div className=" flex justify-between items-center">
+                        <p className=" my-4 text-lg text-gray-300">
+                          ${item.price}
+                        </p>
+                        <p className=" bg-gray-500 hover:bg-gray-400 px-3 py-1  rounded-lg">
+                          Details
+                        </p>
+                      </div>
+                    </div>
+                  </motion.div>
+                </AnimatePresence>
+
+                {/* //View display when Popular is Clicked======================= */}
+
+                {item.popular && (
+                  <AnimatePresence>
+                    <motion.div
+                      whileHover={{ scale: 1.01 }}
+                      key={index}
+                      className={`mb-16 sm:mb-0  ${
+                        isPopularClicked ? "" : "hidden"
+                      }`}
+                    >
+                      <img
+                        className=" xxs:h-[320px] xs:h-[350px] sm:h-[220px] md:h-56 lg:h-52 xl:h-60 w-full rounded-t-xl"
+                        src={item.imgUrl}
+                        alt="product"
+                      />
+                      <div className="  bg-gray-600 px-3 py-3 rounded-b-xl ">
+                        <div>
+                          <p className=" text-md text-gray-400">{item.name}</p>
+                        </div>
+
+                        <div className=" flex justify-between items-center">
+                          <p className=" my-4 text-lg text-gray-300">
+                            ${item.price}
+                          </p>
+                          <p className=" bg-gray-500 hover:bg-gray-400 px-3 py-1  rounded-lg">
+                            Details
+                          </p>
+                        </div>
+                      </div>
+                    </motion.div>
+                  </AnimatePresence>
+                )}
+              </>
+            );
+          }
         })}
-      </section>
+      </motion.section>
     </div>
   );
 };
